a0ce90835895ce5e455939ee718260aa637797c1
Ubuntu <ubuntu@ip-172-31-26-95.us-east-2.compute.internal>
2019-05-02 21:43:29 +0000

    photo1

--- chris/settings.py
+++ chris/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '&r4dxd2kjm%=bdwo4jeth#&ko-z7k%6-8o^3wsj_sk+5!y286w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 EMAIL_HOST='smtp.mail.yahoo.com'
 EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
@@ -37,7 +37,7 @@ print(os.environ.get('EMAIL_HOST_USER'))
 print(os.environ.get('EMAIL_HOST_PASSWORD'))
 print(os.environ.get('SECRET_KEY'))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.222.229.253']
 
 
 # Application definition
@@ -131,3 +131,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
